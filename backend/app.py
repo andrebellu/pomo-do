488c07ba0855fe28877d6cd11e394f4e5c7d4c53
andrebellu <bellu.andrea@outlook.it>
2023-01-27 16:26:41 +0100

    fixed get method

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, json, request
 import mysql.connector
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -21,8 +21,8 @@ def create_cursor():
 
 
 insert_task = ("INSERT INTO tasks "
-               "(titolo, contenuto, `data`, stato) "
-               "VALUES (%s, %s, %s, %s)")
+                "(titolo, contenuto, `data`, stato) "
+                "VALUES (%s, %s, %s, %s)")
 
 
 def add_task():
@@ -36,7 +36,15 @@ def add_task():
 def get_tasks():
     cnx = create_cursor()
     cnx.execute("SELECT * FROM tasks")
-    tasks = cnx.fetchall()
+    tasks = []
+    for (id, titolo, data, contenuto, stato) in cnx:
+        tasks.append({
+            "id": id,
+            "titolo": titolo,
+            "contenuto": contenuto,
+            "data": data,
+            "stato": stato
+        })
     cnx.close()
     return tasks
 
@@ -49,8 +57,7 @@ def hello_world():
 @app.route("/tasks", methods=["GET", "POST"])
 def view_tasks():
     if request.method == "GET":
-        tasks = get_tasks()
-        return json.dumps(tasks), 200, {'ContentType': 'application/json'}
+        return json.dumps(get_tasks()), 200, {'ContentType': 'application/json'}
     elif request.method == "POST":
         add_task()
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
@@ -64,8 +71,16 @@ def delete_task(id):
     cnx.close()
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
-
 @app.route("/tasks/<int:id>", methods=["PUT"])
+def change_task(id):
+    cnx = create_cursor()
+    cnx.execute("UPDATE tasks SET titolo = %s, contenuto = %s, `data` = %s, stato = %s WHERE id = %s", (request.json['titolo'], request.json['contenuto'], request.json['data'], request.json['stato'], id))
+    mydb.commit()
+    cnx.close()
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@app.route("/tasks/<int:id>", methods=["PATCH"])
 def task_done(id):
     cnx = create_cursor()
     cnx.execute("UPDATE tasks SET stato = NOT stato WHERE id = %s", (id,))
