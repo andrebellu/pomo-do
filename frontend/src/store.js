94bddb168ba573b4f5bc45260b198cd27daaab52
Leonardo Baccalario <leonardobaccalario@gmail.com>
2023-01-28 16:46:36 +0100

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,6 @@ import { writable } from "svelte/store";
 
 export const todos = writable([]);
 
-export const url = "http://192.168.0.101:5000/tasks"
+export const url = "http://127.0.0.1:5000/tasks"
 
 export const tomorrow = writable(false);
